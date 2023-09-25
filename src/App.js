1343aa6a48f676a67cc2724097c34beb9c3d5630
ravih12 <45748749+ravih12@users.noreply.github.com>
2023-09-25 22:36:06 +0530

    Day two

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home"
+import Profile from "./Pages/Profile"
+import SingIn from "./Pages/SignIn"
+import SingUp from "./Pages/SignUp"
+import ForgotPassword from "./Pages/ForgotPassword"
+import Offers from "./Pages/Offers"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/sign-in" element={<SingIn />} />
+          <Route path="/sign-up" element={<SingUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/offers" element={<Offers />} />
+
+        </Routes>
+      </Router>
+    </>
   );
 }
 
